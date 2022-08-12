9692aa8799ed664843f8ad4f1750c3001e3a5c40
Fiaz1994 <111137052+Fiaz1994@users.noreply.github.com>
2022-08-12 20:43:41 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,20 @@
+package com.javapractive1;
+
+public class Demo1 {
+	public void apple() {
+		System.out.println("apples");
+	}
+
+	public void orange() {
+		System.out.println("oranges");
+
+	}
+
+	public static void main(String[] args) {
+		Demo1 d = new Demo1();
+		d.apple();
+		d.orange();
+
+	}
+
+}
